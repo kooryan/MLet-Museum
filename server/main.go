b2706d6bb112de0bee83eb9dae47f6e22c742534
kooryan <kooryan03@gmail.com>
2022-01-30 17:52:51 -0600

    Update <password> field in .env file. Basic set up

---
+++ server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.Use(cors.Default())
+
+	//this runs the server and allows it to listen to requests.
+	router.Run(":" + port)
+}
